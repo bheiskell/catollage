e4a082f1d57d3f84c9d661e0fdd5d53b982de33f
Ben Heiskell <bheiskell@factset.com>
2013-04-04 22:45:55 -0400

    Refactoring pretty heavily and adding the web application interface.

--- README.rst
+++ README.rst
@@ -1,13 +1,26 @@
 Catollage
------------
+=========
 
 Create a collage of an image using a set of images! For example, make a cat out of other cats!
 
 .. image:: images/example.jpg
 
-Packages
-~~~~~~~~
+Install
+-------
 
-To utilize virtual environments on Ubuntu, I needed to install the following packages.
+For some reason, pysci is complaining about not having numpy. Although numpy in declared earlier in the setup.py, that doesn't seem to resolve the issue. To fix, install numpy explicitly first.
+
+::
+
+  git clone https://github.com/bheiskell/catollage.git
+  cd catollage
+  virtualenv --no-site-packages venv
+  . venv/bin/activate
+  pip install numpy .
+
+Packages
+--------
+
+To utilize virtual environments on Ubuntu, I needed to install the following packages.::
 
   $ sudo aptitude install build-essential gfortran libamd2.2.0 libblas3gf libc6 libgcc1 libgfortran3 libjpeg libjpeg-dev liblapack-dev liblapack3gf libopenblas-dev libstdc++6 libumfpack5.4.0 python-all-dev python-dev
--- catollage/app.py
+++ catollage/app.py
@@ -1,7 +1,6 @@
-"""
-"""
+"""Catollage web application."""
 
-from flask import Flask
+from flask import Flask, render_template, request, send_file
 from PIL import Image
 from StringIO import StringIO
 from scipy import spatial
@@ -16,136 +15,196 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__) #pylint: disable=C0103
 
-@app.route('/collage/')
-def serve_img():
-    """Serve the requested image."""
-    img = Image.new('RGB')
-    return serve_pil_image(img)
+MAX_CELL_SIZE = 16
+HISTOGRAM_OFFSET = pow(2, 8)
 
-def serve_pil_image(image):
-    """Serve a Flask image stream."""
+@app.route('/')
+def index():
+    """Index page."""
+    return render_template('index.html')
+
+@app.route('/collage/', methods=['POST'])
+def upload():
+    """Handle the uploaded image."""
+
+    uploaded_image = Image.open(request.files['image'])
+
+    __log__.debug("Uploaded image %s", uploaded_image)
+
+    collage_image = app.config['catollage'].collage_from(uploaded_image)
+    return serve_image(collage_image)
+
+def serve_image(image):
+    """Serve a image stream."""
     stream = StringIO()
     image.save(stream, 'JPEG', quality=70)
     stream.seek(0)
-    return send_file(stream, mimetype='image/jpeg')
+    return send_file(stream, mimetype='image/jpeg', cache_timeout=0)
+
+class Sources(object):
+    """Manage and interact with collage sources."""
+
+    def __init__(self):
+        self.rgb_to_images = None
+        self.rgbs = None
+        self.kdtree = None
+
+    def from_dir(self, directory):
+        """Load sources from a directory"""
+        self.from_images(( Image.open(join(directory, filename)) for filename in listdir(directory) ))
+
+    def from_images(self, images):
+        """Load sources from a list of images."""
+        self.rgb_to_images = Sources._get_rgb_to_images(images)
+        self.rgbs = self.rgb_to_images.keys()
+        self.kdtree = spatial.KDTree(self.rgbs)
+
+    def collage_from(self, image):
+        """Convert the image to a collage."""
+
+        # TODO: simply this method
+        #pylint: disable=R0914
+
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+
+        (width, height) = image.size
+
+        __log__.debug("Processing an image of size %dx%d", width, height)
+
+        rgb_to_coords = Sources._get_rgb_to_coords(image, width, height)
+        rgbs = rgb_to_coords.keys()
+
+        (distances, indexes) = self.kdtree.query(rgbs) #pylint: disable=W0612
+
+        new_image = Image.new("RGB", (width, height))
+
+        for i in range(len(rgbs)):
+            rgb = self.rgbs[indexes[i]]
+            source = self.rgb_to_images[rgb]
+
+            for coord in rgb_to_coords[rgbs[i]]:
+                #__log__.debug("Coordinate: %s - Color %s is similar to %s", coord, rgbs[i], rgb)
+
+                (x_min, y_min) = coord
+                range_tuple = Sources._get_x_y_range(x_min, y_min, width, height, MAX_CELL_SIZE)
+
+                new_image.paste(source, range_tuple)
+
+        return new_image
+
+    @staticmethod
+    def _get_rgb_to_images(images):
+        """Get a mapping of RGB tuples to images."""
+        rgb_to_images = {}
+        for image in images:
+            try:
+                if image.mode != "RGB":
+                    image = image.convert("RGB")
+
+                rgb = Sources._get_rgb(image)
+
+                __log__.debug("Color of %s is %s", image, rgb)
+
+                rgb_to_images[rgb] = image.resize((MAX_CELL_SIZE, MAX_CELL_SIZE))
+
+            except IOError, exception:
+                __log__.warn("Failed to load image %s: %s", image, exception)
+
+        return rgb_to_images
+
+    @staticmethod
+    def _get_rgb(image):
+        """Get an RGB tuple that represents this image."""
+
+        histogram_dict = Sources._get_color_to_histogram(image.histogram())
+
+        rgb = {}
+        for color, histogram in histogram_dict.items():
+            weighted_sum = sum(count * histogram[count] for count in range(len(histogram)))
+            num_pixels = sum(histogram)
+            rgb[color] = weighted_sum / num_pixels
 
 
-__OFFSET = pow(2, 8)
+        return (rgb['R'] , rgb['G'], rgb['B'])
 
-def image_average_color(image):
-    histogram_list = image.convert("RGB").histogram()
-    histogram_dict = {
-        'R': histogram_list[__OFFSET * 0:__OFFSET],
-        'G': histogram_list[__OFFSET * 1:__OFFSET * 2],
-        'B': histogram_list[__OFFSET * 2:__OFFSET * 3],
-    }
+    @staticmethod
+    def _get_color_to_histogram(histogram):
+        """Get a dict mapping color to a histogram list."""
+        return {
+            'R': histogram[HISTOGRAM_OFFSET * 0:HISTOGRAM_OFFSET],
+            'G': histogram[HISTOGRAM_OFFSET * 1:HISTOGRAM_OFFSET * 2],
+            'B': histogram[HISTOGRAM_OFFSET * 2:HISTOGRAM_OFFSET * 3],
+        }
 
-    average_color = {}
-    for color, histogram in histogram_dict.items():
-        # http://stackoverflow.com/questions/7563315/how-to-loop-over-histogram-to-get-the-color-of-the-picture
-        weighted_sum = sum(count * histogram[count] for count in range(len(histogram)))
-        num_pixels = sum(histogram)
-        average_color[color] = weighted_sum / num_pixels
+    @staticmethod
+    def _get_rgb_to_coords(image, width, height):
+        """Get a dict mapping rgbs to lists of top-left coordinates."""
 
+        rgb_to_coords = {}
+        for x_cell in range(width // MAX_CELL_SIZE):
+            for y_cell in range(height // MAX_CELL_SIZE):
 
-    return (average_color['R'] , average_color['G'], average_color['B'])
+                (x_min, y_min, x_max, y_max) = Sources._get_x_y_range(
+                    x_cell * MAX_CELL_SIZE,
+                    y_cell * MAX_CELL_SIZE,
+                    width,
+                    height,
+                    MAX_CELL_SIZE
+                )
 
-def get_image_dict(directory, max_cell_size):
-    images = {}
-    for filename in listdir(directory):
-        try:
-            image = Image.open(join(directory,filename))
-            average_color = image_average_color(image)
+                rgb = Sources._get_rgb_of_region(image, x_min, x_max, y_min, y_max)
 
-            __log__.debug("Average color of '%s' is %s", filename, average_color)
+                if rgb not in rgb_to_coords:
+                    rgb_to_coords[rgb] = []
 
-            images[average_color] = image.resize((max_cell_size, max_cell_size))
-        except IOError:
-            pass
+                rgb_to_coords[rgb].append((x_min, y_min))
 
-    return images
+        return rgb_to_coords
 
-def get_average_color(pixels, x_min, x_max, y_min, y_max):
+    @staticmethod
+    def _get_x_y_range(x, y, width, height, cell_size):
+        """Helper for getting region coordinates."""
 
-    count = 0
-    r_sum = 0
-    g_sum = 0
-    b_sum = 0
+        #pylint: disable=C0103
+        return (
+            x,
+            y,
+            min(x + cell_size, width),
+            min(y + cell_size, height),
+        )
 
-    for x in range(x_min, x_max):
-        for y in range(y_min, y_max):
-            (r, g, b) = pixels[x, y]
-            r_sum += r
-            g_sum += g
-            b_sum += b
-            count += 1
+    @staticmethod
+    def _get_rgb_of_region(image, x_min, x_max, y_min, y_max):
+        """Get an RGB tuple that represents an images region."""
 
-    return (
-        int(round(r_sum / count)),
-        int(round(g_sum / count)),
-        int(round(b_sum / count))
-    )
+        count = 0
+        r_sum = 0
+        g_sum = 0
+        b_sum = 0
 
-cell_size = 32
+        #pylint: disable=C0103
+        for x in range(x_min, x_max):
+            for y in range(y_min, y_max):
+                (r, g, b) = image.getpixel((x, y))
+                r_sum += r
+                g_sum += g
+                b_sum += b
+                count += 1
 
-images = get_image_dict(argv[1], cell_size)
-coordinates = images.keys()
+        return (
+            int(round(r_sum / count)),
+            int(round(g_sum / count)),
+            int(round(b_sum / count))
+        )
 
-kdtree = spatial.KDTree(coordinates)
+def main():
+    """Run the web application."""
+    sources = Sources()
+    sources.from_dir(argv[1])
 
-image = Image.open(argv[2])
-pixels = image.convert("RGB").load()
-(width, height) = image.size
+    app.config['catollage'] = sources
+    app.run(host='0.0.0.0', debug=True)
 
-color_to_coord = {}
-for x_cell in range(width // cell_size):
-    for y_cell in range(height // cell_size):
-        x_min = x_cell * cell_size
-        x_max = min(x_min + cell_size, width)
-        y_min = y_cell * cell_size
-        y_max = min(y_min + cell_size, height)
-
-        average_color = get_average_color(pixels, x_min, x_max, y_min, y_max)
-
-        if average_color not in color_to_coord:
-            color_to_coord[average_color] = []
-
-        color_to_coord[average_color].append((x_min, y_min))
-
-        #__log__.debug("Blocks %d:%d:%d:%d - %s", x_min, x_max, y_min, y_max, average_color)
-
-colors = color_to_coord.keys()
-(distances, indexes) = kdtree.query(colors)
-
-new_image = Image.new("RGB", (width, height))
-
-offsets = []
-for i in range(len(colors)):
-    index = indexes[i]
-    coordinate = coordinates[indexes[i]]
-    cell = images[coordinates[indexes[i]]]
-    for offset in color_to_coord[colors[i]]:
-        offsets.append(offset)
-        __log__.debug("At %s color %s is similar to %s", offset, colors[i], coordinate)
-        new_image.paste(cell, offset)
-
-#print set(offsets).difference(set(color_to_coord.values()))
-#print set(color_to_coord.values()).difference(set(offsets))
-
-new_image.save(argv[3])
-
-#http://docs.python.org/2/library/colorsys.html
-#http://stackoverflow.com/questions/500607/what-are-the-lesser-known-but-useful-data-structures
-#http://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.query.html#scipy.spatial.KDTree.query
-#https://en.wikipedia.org/wiki/K-d_tree
-#http://www.pythonware.com/library/pil/handbook/image.htm
-#Loop through all images getting their average rgb or hvs or chom whatever
-#Generate a kd tree
-#
-#Check image size (reject anything over a meg)
-#Pick a pixel size (reject anything that will result in > 1024
-#Loop through each section calculating average
-#Get the average color for that section using the histogram
-#Load the image for that color
-#Resize the image to the block size
-#Overlay it onto the image
+if __name__ == '__main__':
+    main()
